86e7a7a2c2a31039d6f986edc69bb019fca47ab1
Fahrizal Septrianto <ijalcavaliers@yahoo.co.id>
2018-02-18 21:20:24 +0700

    Use jackson mapper for log

--- src/main/kotlin/com/mczal/dialogflow/controller/ApiController.kt
+++ src/main/kotlin/com/mczal/dialogflow/controller/ApiController.kt
@@ -1,9 +1,11 @@
 package com.mczal.dialogflow.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.mczal.dialogflow.dto.request.DataRequest
 import com.mczal.dialogflow.dto.response.DataResponse
 import com.mczal.dialogflow.dto.response.fulfillment.FulfillmentText
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,14 +17,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/dialogflow")
 class ApiController {
 
-  val logger = LoggerFactory.getLogger(this::class.java)
+  private val logger = LoggerFactory.getLogger(this::class.java)
+
+  @Autowired
+  private lateinit var jacksonMapper: ObjectMapper
 
   @PostMapping("",
     consumes = [MediaType.APPLICATION_JSON_VALUE],
     produces= [MediaType.APPLICATION_JSON_VALUE]
   )
   fun handleRequest(@RequestBody content: DataRequest): ResponseEntity<DataResponse>{
-    logger.warn(">>>>> content: $content")
+    logger.warn(">>>>> content request: ${jacksonMapper.writeValueAsString(content)}")
 
     return ResponseEntity.ok(
       DataResponse(
